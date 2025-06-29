0516ae3b6e504d586d8fb41bebadef12f26b7e37
Dnyandip9116 <dnyandip40@gmail.com>
2025-06-29 16:53:04 +0530

    Add files via upload

---
+++ BookStore/BOOK STORE E-COMMERCE SITE/footer.php
@@ -0,0 +1,42 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +91 7558385525 </p>
+            <p> <i class="fas fa-phone"></i> +91 7558385525 </p>
+            <p> <i class="fas fa-envelope"></i> Dnyandip40@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Jalgaon, India </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter" target="_blank"></i>twitter</a>
+            <a href="https://www.instagram.com/mr_nehete_2003/" target="_blank"><i class="fab fa-instagram"></i>instagram</a>
+
+            <a href="#"><i class="fab fa-linkedin" target="_blank"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Dnyandip Nehete</span> </div>
+
+</section>
